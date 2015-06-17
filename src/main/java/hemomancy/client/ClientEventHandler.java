18c026816fdf72e04e46a628acfb7f5208754b04
WayofTime <wtime@live.ca>
2015-06-17 16:49:42 -0400

    Experimentation with rendering a custom overlay around the player - this will be used for the self spells.

--- src/main/java/hemomancy/client/ClientEventHandler.java
+++ src/main/java/hemomancy/client/ClientEventHandler.java
@@ -2,12 +2,20 @@ package hemomancy.client;
 
 import hemomancy.client.hud.HUDRenderHelper;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
 import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import org.lwjgl.opengl.GL11;
 
 @SideOnly(Side.CLIENT)
 public class ClientEventHandler
@@ -30,4 +38,62 @@ public class ClientEventHandler
 	        }
 		} 
     }
+	
+	@SubscribeEvent
+	public void renderLivingEvent(RenderLivingEvent.Pre event)
+	{
+		if(event.entity instanceof EntityPlayer)
+		{
+			long worldTime = event.entity.worldObj.getWorldTime();
+			double tickRate = 100;
+			double cornerX = worldTime / tickRate;
+			double cornerY = worldTime / tickRate;
+			double scaleFactor = 1.0/32.0;
+			
+			double size = 20;
+			
+			Minecraft mc = Minecraft.getMinecraft();
+			
+			GL11.glPushMatrix();
+			
+			GL11.glScaled(scaleFactor, scaleFactor, scaleFactor);
+			OpenGlHelper.glBlendFunc(770, 771, 1, 0);
+			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+			mc.getTextureManager().bindTexture(new ResourceLocation("hemomancy", "textures/gui/BlankBar1.png"));
+			Tessellator tessellator = Tessellator.getInstance();
+			WorldRenderer wr = tessellator.getWorldRenderer();
+			
+			wr.startDrawingQuads();
+			wr.addVertexWithUV(-size/2, size, -size/2, 0.0D + cornerX, 1.0D + cornerY);
+			wr.addVertexWithUV(size/2, size, -size/2, 1.0D + cornerX, 1.0D + cornerY);
+			wr.addVertexWithUV(size/2, 0, -size/2, 1.0D + cornerX, 0.0D + cornerY);
+			wr.addVertexWithUV(-size/2, 0, -size/2, 0.0D + cornerX, 0.0D + cornerY);
+			tessellator.draw();
+			
+			wr.startDrawingQuads();
+			wr.addVertexWithUV(-size/2, 0, size/2, 0.0D + cornerX, 0.0D + cornerY);
+			wr.addVertexWithUV(size/2, 0, size/2, 1.0D + cornerX, 0.0D + cornerY);
+			wr.addVertexWithUV(size/2, size, size/2, 1.0D + cornerX, 1.0D + cornerY);
+			wr.addVertexWithUV(-size/2, size, size/2, 0.0D + cornerX, 1.0D + cornerY);
+			tessellator.draw();
+			
+			wr.startDrawingQuads();
+			wr.addVertexWithUV(-size/2, 0, -size/2, 0.0D + cornerX, 0.0D + cornerY);
+			wr.addVertexWithUV(-size/2, 0, size/2, 1.0D + cornerX, 0.0D + cornerY);
+			wr.addVertexWithUV(-size/2, size, size/2, 1.0D + cornerX, 1.0D + cornerY);
+			wr.addVertexWithUV(-size/2, size, -size/2, 0.0D + cornerX, 1.0D + cornerY);
+			tessellator.draw();
+			
+			wr.startDrawingQuads();
+			wr.addVertexWithUV(size/2, size, -size/2, 0.0D + cornerX, 1.0D + cornerY);
+			wr.addVertexWithUV(size/2, size, size/2, 1.0D + cornerX, 1.0D + cornerY);
+			wr.addVertexWithUV(size/2, 0, size/2, 1.0D + cornerX, 0.0D + cornerY);
+			wr.addVertexWithUV(size/2, 0, -size/2, 0.0D + cornerX, 0.0D + cornerY);
+			tessellator.draw();
+
+			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+			
+			GL11.glPopMatrix();
+		}
+	}
 }
